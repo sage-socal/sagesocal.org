21bda1cdbd152c2021346a030c8be7770fd1eed8
Spencer Schoeben <spencer@netspencer.com>
2019-06-23 15:37:50 -0700

    Updating base site to newest APIs

--- pages/_document.js
+++ pages/_document.js
@@ -1,108 +1,44 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
-import SageGlobalStyle from '../components/GlobalStyle'
-import { Normalize } from 'styled-normalize'
 
-export default class MyDocument extends Document {
-  render() {
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
-    const main = sheet.collectStyles(<Main />)
-    const styleTags = sheet.getStyleElement()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
     return (
-      <html>
-        <Head>
-          <Normalize />
-          <SageGlobalStyle />
-          <title>CASSY SoCal</title>
-
-          <link
-            rel="apple-touch-icon"
-            sizes="57x57"
-            href="/static/favicon/apple-icon-57x57.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="60x60"
-            href="/static/favicon/apple-icon-60x60.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="72x72"
-            href="/static/favicon/apple-icon-72x72.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="76x76"
-            href="/static/favicon/apple-icon-76x76.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="114x114"
-            href="/static/favicon/apple-icon-114x114.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="120x120"
-            href="/static/favicon/apple-icon-120x120.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="144x144"
-            href="/static/favicon/apple-icon-144x144.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="152x152"
-            href="/static/favicon/apple-icon-152x152.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/static/favicon/apple-icon-180x180.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="192x192"
-            href="/static/favicon/android-icon-192x192.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/static/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="96x96"
-            href="/static/favicon/favicon-96x96.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/static/favicon/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/static/manifest.json" />
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta
-            name="msapplication-TileImage"
-            content="/static/favicon/ms-icon-144x144.png"
-          />
-          <meta name="theme-color" content="#ffffff" />
-
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          {styleTags}
-        </Head>
+      <Html lang="en">
+        <Head />
         <body>
-          <div className="root">{main}</div>
+          <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
+
+export default MyDocument
