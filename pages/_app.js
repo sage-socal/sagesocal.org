0f8b50e3d34e63e62cff9b42ce8abc04737c6fe2
Spencer Schoeben <spencer@netspencer.com>
2019-06-23 15:42:29 -0700

    Add files for Favicon and Globalstyles component

---
+++ pages/_app.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import App, { Container } from 'next/app'
+import SageGlobalStyle from '../components/GlobalStyle'
+import { Normalize } from 'styled-normalize'
+import Favicon from '../components/Favicon'
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Container>
+        <Favicon />
+        <Normalize />
+        <SageGlobalStyle />
+        <Component {...pageProps} />
+      </Container>
+    )
+  }
+}
