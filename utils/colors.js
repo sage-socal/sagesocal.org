6819dc3828106b54c21feda97446396f9fcb9218
Spencer Schoeben <spencer@netspencer.com>
2021-06-19 20:14:50 -0700

    fix javascript

--- utils/colors.js
+++ utils/colors.js
@@ -7,8 +7,8 @@ export const yellow = '#FFE74C'
 export const chalk = '#E7E5E5'
 export const stone = '#404E4D'
 
-export const dark = black
-export const light = darken(0.4, chalk)
-
 export const white = '#FFFFFF'
 export const black = '#000000'
+
+export const dark = black
+export const light = darken(0.4, chalk)
