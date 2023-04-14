9d4310ab03987d2b37336de00d2c368bee82bacb
Spencer Schoeben <spencer@netspencer.com>
2023-04-13 19:49:24 -0700

    remove unncessary lamba

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,6 @@ import { Normalize } from 'styled-normalize'
 import Favicon from '../components/Favicon'
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
   render() {
     const { Component, pageProps } = this.props
 
